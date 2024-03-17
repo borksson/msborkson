9d37f5b5a59529f7f1dbf7d51e52642830b1a928
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2024-03-17 17:38:50 -0600

    Docker working with registry

--- server/main.go
+++ server/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"os"
 	"github.com/sirupsen/logrus"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
   "github.com/toorop/gin-logrus"
+	"github.com/joho/godotenv"
 )
 
 func init() {
@@ -14,10 +18,32 @@ func init() {
 	logrus.SetLevel(logrus.DebugLevel)
 }
 
-func main() {
-	port := os.Getenv("PORT")
+func proxy(c *gin.Context, remote *url.URL) {
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = c.Param("proxyPath")
+	}
 
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
+
+func main() {
 	log := logrus.New()
+
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+	port := os.Getenv("PORT")
+	registry_url, err := url.Parse(os.Getenv("REGISTRY_URL"))
+	if err != nil {
+		panic(err)
+	}
 
 	r := gin.Default()
 	r.Use(ginlogrus.Logger(log), gin.Recovery())
@@ -28,6 +54,10 @@ func main() {
 			"message": "pong",
 		})
 	});
+
+	r.Any("/registry/*proxyPath", func(c *gin.Context) {
+		proxy(c, registry_url)
+	})
 
 	r.Run(":" + port)
 }
